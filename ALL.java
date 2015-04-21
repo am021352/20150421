db8735b890790a137003765ffdfa517f06013ed1
AM021355 <a0912481437@gmail.com>
2015-04-21 21:48:39 +0800

    develope

---
+++ ALL.java
@@ -0,0 +1,11 @@
+import java.until.Scanner;
+
+public class ALL
+{
+	public static void main(String[] args)
+	{
+		System.out.println("CSIE");
+		System.out.println("AM021355");
+		System.out.println("Robert");
+	}
+}
