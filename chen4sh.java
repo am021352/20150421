3b1979ef2a208904b6ea7330838975e9e52d8937
am021352 <clovercry123@gmail.com>
2015-04-21 22:01:28 +0800

    AM021352

---
+++ chen4sh.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class chen4sh{
+
+
+	void am021352(){
+
+		Scanner input=new Scanner(System.out);
+		int a;
+		int b;
+		a=input.nextInt();	
+		b=input.nextInt();
+		if(a>b)
+			System.out.println(a+">"+b);
+		if(a<b)
+			System.out.println(b+">"+a);
+		if(a==b)
+			System.out.println(a+"="+b);
+		
+
+
+
+	}
+
+
+}
